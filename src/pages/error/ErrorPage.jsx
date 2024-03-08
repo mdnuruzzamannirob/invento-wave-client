3bb0085e83dee35117f169e0deb11a87ce9257fa
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-03-08 23:32:36 +0600

    updated the not found page

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -5,10 +5,12 @@ const ErrorPage = () => {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <img src={gif} alt="" />
-      <h3 className="text-3xl text-center">page not found</h3>
+      <h3 className="text-5xl text-center font-semibold">
+        sorry, page not found !!!
+      </h3>
       <Link
         to={"/"}
-        className="btn rounded-md mt-10  bg-pink-600 hover:bg-pink-700 border-none text-white"
+        className="btn btn-sm h-10 px-5 rounded-md mt-10  bg-pink-600 hover:bg-pink-700 border-none text-white"
       >
         Go Home
       </Link>
